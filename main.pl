5874fe9d4f2ddd7a1ee75af8dac1f967a4facf64
JosiasBatista <josias.leal@ccc.ufcg.edu.br>
2021-10-08 00:06:14 -0300

    Gerenciando casos de erro para a busca de jogadores para a partida

--- main.pl
+++ main.pl
@@ -6,7 +6,7 @@ save(Players,Matches):-
 repeatFlow(Repeat,_,_):- Repeat == false, halt.
 repeatFlow(Repeat,Players,Matches):- Repeat == true, write("\n\n"), initialFlow(Repeat,Players,Matches).
 
-initialFlow(Repeat,PlayerList,MatchList):- 
+initialFlow(Repeat,PlayerList,MatchList):-
   showMainMenu,
   read_string(user, ".", "\n", _, Option),
   controlFlow(Option,Repeat,PlayerList,MatchList),
@@ -24,7 +24,7 @@ controlFlow(Option,Repeat,PlayerList,MatchList):-
   Repeat = true,
   ( Option =:= "1" -> addPlayer(PlayerList, MatchList) ;
     Option =:= "2" -> addMatch(PlayerList, MatchList) ;
-    Option =:= "3" -> searchPlayer ;
+    Option =:= "3" -> searchForPlayer ;
     Option =:= "4" -> comparePlayers ;
     Option =:= "5" -> repeatFlow(false,_,_)
   ).
@@ -42,41 +42,54 @@ showAddPlayerMenu(Name):-
   
 addMatch(PlayerList, MatchList):- 
   write("Adicionando uma nova partida!\n"),
-  showAddMatchMenu(PlayerOneID, PlayerTwoID, PlayerList, MatchList).
+  showAddMatchMenu(PlayerList, MatchList).
 
-showAddMatchMenu(PlayerOneID, PlayerTwoID, PlayerList, MatchList):-
-  searchPlayerOneID(PlayerList, PlayerOneID),
-  searchPlayerTwoID(PlayerList, PlayerTwoID),
+showAddMatchMenu(PlayerList, MatchList):-
+  searchPlayerOneID(PlayerList, PlayerOneID, MatchList),
+  searchPlayerTwoID(PlayerList, PlayerTwoID, MatchList),
   defineTheWinner(Winner),
   append(MatchList, [[PlayerOneID, PlayerTwoID, Winner]], NewList),
-  write(NewList),
+  write("Partida adicionada com sucesso!\n\n"),
   initialFlow(true, PlayerList, NewList).
 
 defineTheWinner(Winner):-
-  write("Informe a cor das peças do jogador vencedor(b - Branca | p - Preta): \n"),
+  write("Informe a cor das peças do jogador vencedor(B - Branca | P - Preta): \n"),
   read_string(user, ".", "\n", _, Winner).
   
-searchPlayerTwoID(PlayerList, PlayerTwoID):-
+searchPlayerTwoID(PlayerList, PlayerTwoID, MatchList):-
   write("Informe o nome do jogador com peças pretas: \n"),
   read_string(user, ".", "\n", _, Name),
-  searchPlayer(Name, PlayerList, PlayerTwo),
-  setPlayerTwoID(PlayerTwoID, PlayerTwo).
+  searchPlayerTwo(Name, PlayerList, PlayerTwo, SearchStatus),
+  SearchStatus == false ->
+    initialFlow(true, PlayerList, MatchList) ;
+    setPlayerTwoID(PlayerTwoID, PlayerTwo).
 
-searchPlayerOneID(PlayerList, PlayerOneID):-
+searchPlayerOneID(PlayerList, PlayerOneID, MatchList):-
   write("Informe o nome do jogador com peças brancas: \n"),
   read_string(user, ".", "\n", _, Name),
-  searchPlayer(Name, PlayerList, Player),
-  setPlayerOneID(PlayerOneID, Player).
+  searchPlayerOne(Name, PlayerList, PlayerOne, SearchStatus),
+  SearchStatus == false ->
+    initialFlow(true, PlayerList, MatchList) ;
+    setPlayerOneID(PlayerOneID, PlayerOne).
 
-setPlayerOneID(PlayerOneID, [H|[ID|Rest]]):-
+setPlayerOneID(PlayerOneID, [_|[ID|_]]):-
   PlayerOneID = ID.
 
-setPlayerTwoID(PlayerTwoID, [H|[ID|Rest]]):-
+setPlayerTwoID(PlayerTwoID, [_|[ID|_]]):-
   PlayerTwoID = ID.
 
-searchPlayer(Name,[],Player,_):- write("Jogador com esse nome não foi encontrado!").
-searchPlayer(Name,[[PName|Rest]|T],Player):-
-  Name == PName -> Player = [PName|Rest] ; searchPlayer(Name, T, Player).
+searchPlayerOne(_, [], _, SearchStatus):- 
+  write("Jogador com esse nome não foi encontrado!\n\n"),
+  SearchStatus = false.
+searchPlayerOne(Name, [[PName|Rest]|T], PlayerOne, SearchStatus):-
+  Name == PName -> PlayerOne = [PName|Rest] ; searchPlayerOne(Name, T, PlayerOne, SearchStatus).
 
-searchPlayer:- write("Search player").
+searchPlayerTwo(_, [], _, SearchStatus):- 
+  write("Jogador com esse nome não foi encontrado!\n\n"),
+  SearchStatus = false.
+searchPlayerTwo(Name,[[PName|Rest]|T],PlayerTwo,SearchStatus):-
+  Name == PName -> PlayerTwo = [PName|Rest] ; searchPlayerTwo(Name, T, PlayerTwo, SearchStatus).
+
+
+searchForPlayer:- write("Search player").
 comparePlayers:- write("Compare players").
