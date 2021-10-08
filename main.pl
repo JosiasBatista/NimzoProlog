5b5f592610ec29d13ab18ebcf235b66498757c56
JosiasBatista <josias.leal@ccc.ufcg.edu.br>
2021-10-07 21:09:53 -0300

    Ajustando a adição de jogadores na lista e ajustando fluxos

--- main.pl
+++ main.pl
@@ -1,6 +1,4 @@
-main:- initialFlow(Repeat, Players, Matches),
-       save(Players,Matches),
-       repeatFlow(Repeat,Players,Matches).
+main:- initialFlow(true, [], []).
 
 save(Players,Matches):-
   write(Players, Matches).
@@ -9,6 +7,7 @@ repeatFlow(Repeat,_,_):- Repeat == false, halt.
 repeatFlow(Repeat,Players,Matches):- Repeat == true, write("\n\n"), initialFlow(Repeat,Players,Matches).
 
 initialFlow(Repeat,PlayerList,MatchList):- 
+  write(PlayerList),
   showMainMenu,
   read_string(user, ".", "\n", _, Option),
   controlFlow(Option,Repeat,PlayerList,MatchList),
@@ -22,29 +21,25 @@ showMainMenu:- write("Bem vindo ao Nimzo \n"),
                  write("4 - Comparar dois jogadores \n"),
                  write("5 - Sair do programa \n").
 
-controlFlow(Option,Repeat,PlayerList,_):-
+controlFlow(Option,Repeat,PlayerList,MatchList):-
   Repeat = true,
-  ( Option =:= "1" -> addPlayer(PlayerList) ;
+  ( Option =:= "1" -> addPlayer(PlayerList, MatchList) ;
     Option =:= "2" -> addMatch ;
     Option =:= "3" -> searchPlayer ;
     Option =:= "4" -> comparePlayers ;
-    Option =:= "5" -> Repeat = false
+    Option =:= "5" -> repeatFlow(false,_,_)
   ).
 
-addPlayer(PlayerList):- 
+addPlayer(PlayerList, MatchList):- 
   write("Adicionando um novo jogador!\n"),
   showAddPlayerMenu(Name),
-  addPlayer(PlayerList,Name),
-  write(PlayerList).
+  length(PlayerList, Length),
+  append(PlayerList, [[Name, Length, 1200]], NewList),
+  initialFlow(true, NewList, MatchList).
 
 showAddPlayerMenu(Name):-
   write("Informe o nome do jogador que deseja adicionar: \n"),
   read_string(user, ".", "\n", _, Name).
-
-addPlayer([H|T],Name):-
-  length([H|T],Lenght),
-  append(T, [[Name]], NewList),
-  [H|T] = NewList.
   
 addMatch:- write("Adding match").
 searchPlayer:- write("Search player").
